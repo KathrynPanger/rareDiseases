5fe516f20adaef75534a418d5d6e0ad3c589e11c
Kathryn <kpanger88@gmail.com>
2022-12-14 19:39:35 -0600

    main page navigation

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 PATH = "/home/ygael/Code/rareDiseases/chromedriver"
-driver = webdriver.Chrome(PATH)
-url = "https://rarediseases.org/for-patients-and-families/information-resources/rare-disease-information/"
-driver.get(url)
-rdr-one-title
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, executable_path=PATH)
+
+#Open Browser
+mainPage = "https://rarediseases.org/for-patients-and-families/information-resources/rare-disease-information/"
+driver.get(mainPage)
+
+#Close Annoying Popup
+xButtonClass = "popmake-close"
+xButtons = driver.find_elements(By.CLASS_NAME, xButtonClass)
+xButtons[0].click()
+
+#Get links to search result pages
+
+
+#Get all result links on each page
+
+
